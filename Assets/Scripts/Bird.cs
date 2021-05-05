437395d0661711bc6c54a10f984ceb5d044365de
Liam Arbuckle <droidology@allianceofdroids.org.au>
2021-05-05 08:55:03 +0800

    UPK-21 AddForce/Launching to bird

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -1,9 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Bird : MonoBehaviour
 {
+    Vector3 _initialPosition; // Only be accessed inside Bird class
+    [SerializeField] private float _launchPower = 250;
+
+    // Resetting scene based on velocity
+    private bool birdWasLaunched;
+    private float timeSittingAround;
+
+    private void Awake() {
+        _initialPosition = transform.position; // Only set on startup
+
+    }
+
+    private void Update() 
+    {
+        if (birdWasLaunched && GetComponent<Rigidbody2D>().velocity.magnitude <= 0.1) // Reset the scene once bird gameobject stops moving after launch
+        {
+            timeSittingAround += Time.deltaTime; // Amount of time since last frame - Time.deltaTime
+        }
+
+        if (transform.position.y > 10 || transform.position.y < -10 || transform.position.x > 10 || transform.position.x < -10 || timeSittingAround > 3) // Reset to default scene if object moves too high, low, or too far to the right or left
+        {
+            string currentSceneName = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(currentSceneName);
+        }
+    }
+
     private void OnMouseDown()
     {
         GetComponent<SpriteRenderer>().color = Color.red;
@@ -11,7 +38,11 @@ public class Bird : MonoBehaviour
 
     private void OnMouseUp()
     {
-        GetComponent<SpriteRenderer>().color = Color.white;
+        GetComponent<SpriteRenderer>().color = Color.white; 
+        Vector2 directionToInitialPosition = _initialPosition - transform.position;
+        GetComponent<Rigidbody2D>().AddForce(directionToInitialPosition * _launchPower);
+        GetComponent<Rigidbody2D>().gravityScale = 1;
+        birdWasLaunched = true;
     }
 
     private void OnMouseDrag() {
